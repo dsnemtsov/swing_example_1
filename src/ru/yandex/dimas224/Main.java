980434a453df3a873b96f27802980fef8b20026d
Dmitrii Nemtsov <dimas224@yandex.ru>
2022-05-21 20:21:52 +0300

    ActionListener

--- src/ru/yandex/dimas224/Main.java
+++ src/ru/yandex/dimas224/Main.java
@@ -1,11 +1,27 @@
 package ru.yandex.dimas224;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame jFrame = MyFrame.getFrame();
 
-        jFrame.add(new MyFrame.MyComponent());
+        JPanel jPanel = new JPanel();
+        jFrame.add(jPanel);
+
+        JButton submit = new JButton("Submit");
+        jPanel.add(submit);
+        submit.addActionListener(e -> {
+            if (jPanel.getBackground().equals(Color.GREEN)) {
+                jPanel.setBackground(Color.YELLOW);
+            } else if (jPanel.getBackground().equals(Color.YELLOW)) {
+                jPanel.setBackground(Color.RED);
+            } else {
+                jPanel.setBackground(Color.GREEN);
+            }
+        });
+
+        //jFrame.add(new MyFrame.MyComponent());
     }
 }
